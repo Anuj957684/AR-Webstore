92d87bd9dfd9227cd9a3cd26cf60bc9f41c45d8b
anujkumar@androwebstech.com <anujkumar@androwebstech.com>
2025-02-05 16:40:37 +0530

    updated

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="contact" id="contact">
+      <div className="main-content">
+        <div class="contact-content">
+          <Link to="/">  </Link>
+          <Link
+            to="/about"
+          >
+            
+          </Link>
+          <Link to="/">  </Link>
+          <Link to="/">  </Link>
+        </div>
+
+        <div className="contact-content">
+          <Link to="/">  </Link>
+          <Link to="/">  </Link>
+          <Link to="/">  </Link>
+        </div>
+
+        <div className="contact-content">
+          <Link to="/feedback">  </Link>
+          <Link to="mailto:shwetkhatri2001@gmail.com" target="_blank">
+           
+          </Link>
+          <Link to="/"></Link>
+        </div>
+
+        <div className="contact-content">
+          <Link
+            to=""
+            target="_blank"
+          >
+            
+          </Link>
+          <Link
+            to=""
+            target="_blank"
+          >
+           
+          </Link>
+          <Link to="" target="_blank">
+           
+          </Link>
+        </div>
+      </div>
+{/* 
+      <div class="action">
+        <form onSubmit={(event) => event.preventDefault()}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            required
+          ></input>
+          <input type="submit" name="submit" value="Submit" required></input>
+        
+        </form>
+      </div> */}
+      <div class="last">
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
